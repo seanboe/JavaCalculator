706038124a4c074f8bc249c114fa24d56f7d4acf
Sean Boerhout <boeseany@gmail.com>
2022-05-12 09:15:52 -0700

    graphing updates

--- src/Graphing.java
+++ src/Graphing.java
@@ -9,13 +9,15 @@ import javax.swing.JPanel;
 
 public class Graphing extends JPanel {
 	
-	private final int frameWidth = 700;
-	private final int frameHeight = 700; 
+	private final int FRAME_WIDTH = 700;
+	private final int FRAME_HEIGHT = 700; 
 
-  private final int ORIGIN_X = frameWidth / 2;
-  private final int ORIGIN_Y = frameHeight / 2;
+  private final int HALF_WIDTH = FRAME_WIDTH / 2;
+  private final int HALF_HEIGHT = FRAME_HEIGHT / 2;
 
+  // A pan to the right (screen moves left) is a negative pan offset, since the screen moves in the negative direction
   private int screenPanOffsetX = 0;
+  // A pan to the up (screen moves down) is a negative pan offset, since the screen moves in the negative direction
   private int screenPanOffsetY = 0;
   
   private Function function; 
@@ -23,7 +25,7 @@ public class Graphing extends JPanel {
 	public Graphing() {
 		// NEED TO INSTANTIATE THE MEMBER VARIABLE function 
 		JFrame f = new JFrame("Graphing");
-		f.setSize(new Dimension(frameWidth, frameHeight));
+		f.setSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
 		f.setBackground(Color.black); 
 		f.add(this);
 		f.setResizable(false);
@@ -32,44 +34,96 @@ public class Graphing extends JPanel {
 		f.setVisible(true);
 	}
 
-	public void paint(Graphics g) { // WITH RESPECT TO ORIGIN (AS OF NOW) 
-		
-		g.setColor(Color.black); 
-		g.fillRect(0, 0, frameWidth, frameHeight); 
+
+  public void paint(Graphics g) {
+
+    final double stepSize = 1;
 
     g.setColor(Color.white);
-    
-///////////////////////////////////////////////////////////////////////////////////////////////////
-    // GRAPHING 
-    double pointX = 0;
-    double pointY = ORIGIN_Y; 
+    g.fillRect(0, 0, FRAME_WIDTH, FRAME_WIDTH);
+    paintAxis(g, true, true);
 
-    // POINT SHOULD BE CIRCLE AND HAVE DIAMETER OF 2 
+    g.setColor(Color.black);
 
-    while (pointX < frameWidth) {
-      HashMap<String, Double> point = new HashMap<String, Double>(); 
-      point.put("x", pointX - frameWidth / 2); 
+    double prevX = 0;
+    double prevY = functionExample(prevX);
+    for (double x = -HALF_WIDTH; x < HALF_WIDTH; x += stepSize) {
+      double computation = functionExample(x);
 
-      double computation = 0.0;
+      // scaling everything properly
+      double coordX = x + HALF_WIDTH;
+      computation = HALF_HEIGHT - computation;
 
-        computation = functionExample(point);
-        System.out.println(computation);
-
-      pointY = (frameHeight / 2) - computation;
-
-      g.fillOval((int)Math.round(pointX), (int)Math.round(pointY), 2, 2); 
-      pointX++; 
-
+      // g.fillOval((int)(coordX), (int)(computation), 2, 2);
+      g.drawLine((int)prevX, (int)prevY, (int)coordX, (int)computation);
+      prevX = coordX;
+      prevY = computation;
     }
-    
-///////////////////////////////////////////////////////////////////////////////////////////////////
+  }
 
-    
+  public void paintAxis(Graphics g, boolean gridLines, boolean dashed) {
+
+    // x axis
+    g.setColor(Color.orange);
+
+    for (int y = 0; y < FRAME_HEIGHT; y++) {
+      if (y % 15 == 0) {
+        for (int x = 0; x < FRAME_WIDTH; x++) {
+          if (x % 4 == 0 && dashed)
+            g.drawOval(x, y, 1, 1);
+        }
+      }
+      if (!gridLines)
+        break;
+    }
+
+    for (int y = 0; y < FRAME_HEIGHT; y++) {
+      if (y % 4 == 0 && dashed)
+        g.drawOval(HALF_WIDTH, y, 1, 1);
+    }
+
+  }
+
+
+
+
+	// public void paint(Graphics g) { // WITH RESPECT TO ORIGIN (AS OF NOW) 
 		
-	}
+	// 	g.setColor(Color.black); 
+	// 	g.fillRect(0, 0, frameWidth, frameHeight); 
 
-  public double functionExample(HashMap<String, Double> point) {
-     return Math.pow(point.get("x"), 1);
+  //   g.setColor(Color.white);
+    
+// ///////////////////////////////////////////////////////////////////////////////////////////////////
+//     // GRAPHING 
+//     double pointX = 0;
+//     double pointY = ORIGIN_Y; 
+
+//     // POINT SHOULD BE CIRCLE AND HAVE DIAMETER OF 2 
+
+//     while (pointX < frameWidth) {
+//       HashMap<String, Double> point = new HashMap<String, Double>(); 
+//       point.put("x", pointX - frameWidth / 2); 
+
+//       double computation = 0.0;
+
+//         computation = functionExample(point);
+//         System.out.println(computation);
+
+//       pointY = (frameHeight / 2) - computation;
+
+//       g.fillOval((int)Math.round(pointX), (int)Math.round(pointY), 2, 2); 
+//       pointX++; 
+
+//     }
+    
+// ///////////////////////////////////////////////////////////////////////////////////////////////////
+		
+// 	}
+
+  public double functionExample(double x) {
+    return x * Math.pow(Math.sin(x / 10), 3);
+
     //return Math.log(point.get("x"));
     //return Math.pow(point.get("x"), 3); 
   }
