a38baf63fd7421d557b9b17498eff928b3c9d6dc
t3chstop <84115793+t3chstop@users.noreply.github.com>
2022-05-24 08:51:56 -0700

    stash

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		Function f1 = new Function("x");
+		Function f2 = new Function(2);
+		Function f = new Function(f1, f2, "+");
+
+		System.out.println(f.compute(5));
+
+
 		
 		
 		// HashMap<String, Function> functions = new HashMap<String, Function>();
@@ -39,33 +47,33 @@ public class Main {
     // Function num2 = new Function(2);
     // Function func = new Function(num1, num2, "^");
 
-    Function a = new Function("x");
-    Function b = new Function(3);
-    Function c = new Function(a, b, "^");
-    Function d = new Function(10);
-    Function e = new Function(c, d, "-");
-    Function f = new Function(3);
-    Function g = new Function(e, f, "^");
+    // Function a = new Function("x");
+    // Function b = new Function(3);
+    // Function c = new Function(a, b, "^");
+    // Function d = new Function(10);
+    // Function e = new Function(c, d, "-");
+    // Function f = new Function(3);
+    // Function g = new Function(e, f, "^");
 
 
-    try {
-      System.out.println(Algebra.bisectionZeros(g, -10, 10));
-    } catch (InvalidInputsException h) {
-      // TODO Auto-generated catch block
-      h.printStackTrace();
-    }
+    // try {
+    //   System.out.println(Algebra.bisectionZeros(g, -10, 10));
+    // } catch (InvalidInputsException h) {
+    //   // TODO Auto-generated catch block
+    //   h.printStackTrace();
+    // }
 
-    Function q = new Function("x");
-    Function w = new Function(2);
-    Function func = new Function(q, w, "^");
+    // Function q = new Function("x");
+    // Function w = new Function(2);
+    // Function func = new Function(q, w, "^");
 
-    try {
-      System.out.println(Algebra.computeZeros(func, 2));
-    } catch (Exception asdf) {
-      asdf.printStackTrace();
-    }
+    // try {
+    //   System.out.println(Algebra.computeZeros(func, 2));
+    // } catch (Exception asdf) {
+    //   asdf.printStackTrace();
+    // }
 		
-	}
+	// }
 	
 	
 	
@@ -125,4 +133,5 @@ public class Main {
 	// public static boolean isOperator(String a) {
 	// 	return true;
 	// }
+	}
 }
